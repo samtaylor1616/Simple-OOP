5004ec972622d213115fb1a03dd516911e2f09ee
Sam Taylor <sammi1616@live.com>
2020-03-10 10:28:12 -0700

    Cleaned up code in engine class

--- Engine.java
+++ Engine.java
@@ -1,109 +1,94 @@
-/* ****************************** *
- * Filename: Engine.java          *
- * Author: Samantha Taylor        *
- * Date last modified: 26/05/2019 *
- * ****************************** */
-
 public class Engine {
-    // Class contants
-    public static final String[] fuelTypes = {"BATTERY", "DIESEL", "BIO"};
+    public static final String[] FUEL_TYPES = { "BATTERY", "DIESEL", "BIO" };
     public static final int MIN_CYL = 2;
     public static final int MAX_CYL = 20;
 
-    // Classfields
     private String fuel;
-    private int cylinders;
+    private int numCylinders;
 
-    // ***************************** CONSTRUCTORS ******************************** //
-    // Default Constructor
     public Engine() {
-        setFuel(fuelTypes[0]);
-        setCylinders(MIN_CYL);
+        setFuel(FUEL_TYPES[0]);
+        setNumCylinders(MIN_CYL);
     }
 
-    // Alternate Constructor
     public Engine(String fuel, int cylinders) {
         try {
             setFuel(fuel);
-            setCylinders(cylinders);
-        }
-        catch(IllegalArgumentException e) {
-            throw new IllegalArgumentException("Error constructing engine: " 
-                                                + e.getMessage());
+            setNumCylinders(cylinders);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Error constructing engine: " + e.getMessage());
         }
     }
 
-    // Copy Constructor
     public Engine(Engine inEngine) {
-        if(inEngine != null) {
+        if (inEngine != null) {
             fuel = inEngine.getFuel();
-            cylinders = inEngine.getCylinders();
-        }
-        else {
+            numCylinders = inEngine.getNumCylinders();
+        } else {
             throw new IllegalArgumentException("Error construcing copy of engine: engine is null");
         }
     }
 
-    // Returns a new Engine object containing the same information as this object
     public Engine clone() {
         return new Engine(this);
     }
 
-
-    // ******************************* SETTERS ********************************** //
     public void setFuel(String fuel) {
-        if(!validateFuel(fuel) ) {
+        if (!isValidFuel(fuel)) {
             throw new IllegalArgumentException(fuel + " is not a valid fuel\n");
         }
         this.fuel = fuel;
     }
-    public void setCylinders(int cylinders) {
-        if(!validateCylinders(cylinders)) {
-            throw new IllegalArgumentException(cylinders + " is not a valid number of cylinders\n");
+
+    public void setNumCylinders(int numCylinders) {
+        if (!isValidNumCylinders(numCylinders)) {
+            throw new IllegalArgumentException(numCylinders + " is not a valid number of cylinders\n");
         }
-        this.cylinders = cylinders;
+        this.numCylinders = numCylinders;
     }
 
-    // ******************************* GETTERS ********************************** //
-    public String getFuel() {return fuel;}
-    public int getCylinders() {return cylinders;}
+    public String getFuel() {
+        return fuel;
+    }
 
-    // **************************** OTHER METHODS ******************************* //
+    public int getNumCylinders() {
+        return numCylinders;
+    }
+
     public boolean equals(Object inObj) {
-        boolean same = false;
-        if(inObj instanceof Engine) {
+        if (inObj instanceof Engine) {
             Engine inEng = (Engine) inObj;
-            if( (fuel.equals(inEng.getFuel()) && (cylinders == inEng.getCylinders()) )) {
-                same = true;
+            if ((fuel.equals(inEng.getFuel()) && (numCylinders == inEng.getNumCylinders()))) {
+                return true;
             }
         }
-        return same;
-    }
-    public String toString() {
-        return "\n\tEngine fuel: " + fuel + "\n\tNumber of cylinders in Engine: " + cylinders;
-    }
-    public String toCSV() {
-        return cylinders + "," + fuel;
+        return false;
     }
 
-    // ************************** VALIDATION METHODS ***************************** //
-    private boolean validateFuel(String fuel) {
-        boolean valid = false;
-        fuel = fuel.toUpperCase();
-         if( fuel != null ) {
-             for( String valFuel : fuelTypes ) {
-                 if(valFuel.equals(fuel)) {
-                     valid = true;
-                 }
-             }
-         }
-        return valid;
+    public String toString() {
+        return "\n\tEngine fuel: " + fuel + "\n\tNumber of cylinders in Engine: " + numCylinders;
     }
-    private boolean validateCylinders(int cylinders) {
-        boolean valid = false;
-        if( (MIN_CYL <= cylinders) && (cylinders <= MAX_CYL)) {
-            valid = true;
+
+    public String toCSV() {
+        return numCylinders + "," + fuel;
+    }
+
+    private boolean isValidFuel(String fuel) {
+        if (fuel != null) {
+            fuel = fuel.toUpperCase();
+            for (String valFuel : FUEL_TYPES) {
+                if (valFuel.equals(fuel)) {
+                    return true;
+                }
+            }
         }
-        return valid;
+        return false;
+    }
+
+    private boolean isValidNumCylinders(int numCylinders) {
+        if ((MIN_CYL <= numCylinders) && (numCylinders <= MAX_CYL)) {
+            return true;
+        }
+        return false;
     }
 }
